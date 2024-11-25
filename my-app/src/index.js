3e82dd2c9ed02304dc8a2ac3437c7084f88455d6
songminho <hyungju911@gamil.com>
2024-11-26 00:37:29 +0900

    배포

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import { Router } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
+      <Router basename='/Project1'>
       <App />
+      </Router>
     </ChakraProvider>
   </React.StrictMode>
 );
