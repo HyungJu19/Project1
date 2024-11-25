759682041d7e2a93fe033e2063e10345b0ce5a11
songminho <hyungju911@gamil.com>
2024-11-26 01:42:23 +0900

    배포2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutPage />} />
 
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
