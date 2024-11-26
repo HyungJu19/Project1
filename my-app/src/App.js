12302e78250639a5c4876d853cfa2e559fbf92d3
songminho <hyungju911@gamil.com>
2024-11-27 00:40:53 +0900

    배포4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {BrowserRouter as  Route,  BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
 
 import BannerPage from './pages/BannerPage';
 import ButtonPage from './pages/ButtonPage';
 import ToglePage from './pages/ToglePage';
 import TextBox from './pages/TextBox';
 import Ba from './pages/Ba';
-import Main from './pages/Main';
 
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-
+      <Routes>
        <Route path='/' element={<Main />} />
         <Route path='/banner' element={<BannerPage />} />
         <Route path='/button' element={<ButtonPage />} />
@@ -20,7 +19,7 @@ function App() {
         <Route path='/textbox' element={<TextBox />} />
         <Route path='/ba' element={<Ba />} />
   
-   
+      </Routes>
     </BrowserRouter>
   );
 }
