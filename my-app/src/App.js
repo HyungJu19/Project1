a8aa63179ea6d9a2d7747046dd81a3e754977e6a
songminho <hyungju911@gamil.com>
2024-11-27 22:59:50 +0900

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as  Route, BrowserRouter, Routes} from 'react-router-dom';
 
 import BannerPage from './pages/BannerPage';
 import ButtonPage from './pages/ButtonPage';
@@ -12,14 +12,14 @@ import Main from './pages/Main';
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      
+        <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/banner' element={<BannerPage />} />
         <Route path='/button' element={<ButtonPage />} />
         <Route path='/togle' element={<ToglePage />} />
         <Route path='/textbox' element={<TextBox />} />
         <Route path='/ba' element={<Ba />} />
-  
+        </Routes>
 
     </BrowserRouter>
   );
